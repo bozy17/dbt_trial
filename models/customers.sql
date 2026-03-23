76fa4c4738994a18909193049145a03526044887
austin.bossart@gmail.com <austin.bossart@gmail.com>
2026-03-23 20:53:40 +0000

    modularity v1

--- models/customers.sql
+++ models/customers.sql
@@ -1,15 +1,6 @@
-with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from dbt-tutorial.jaffle_shop.customers
-
-),
-
-orders as (
+with orders as (
 
     select
         id as order_id,
