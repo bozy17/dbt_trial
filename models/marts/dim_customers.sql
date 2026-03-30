bbe0c19904920e5f838b600805af6282aa2dc501
Austin Bossart <austin.bossart@gmail.com>
2026-03-30 21:57:10 +0000

    Recreate from github

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,7 +6,7 @@ select * from {{ ref('stg_jaffle_shop__customers') }}
 
 orders as (
 
-select * from {{ ref('stg_jaffle_shop__orders') }}
+select * from {{ ref('fct_orders') }}
 
 ),
 
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,24 +1,11 @@
-with 
+select
 
-source as (
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount as payment_amount,
+    created as payment_created,
+    _batched_at
 
-    select * from {{ source('stripe', 'payment') }}
-
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount/100 as amount,
-        created as created_at,
-        _batched_at
-
-    from source
-
-)
-
-select * from renamed
+from dbt-tutorial.stripe.payment
